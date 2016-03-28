dc988ffc25d7e60ccb93c7d0c3158d3a2890eefe
andrey <ac3@cin.ufpe.br>
2016-03-28 11:30:00 -0300

    Adicionando sql mensal pontuacao teste

--- findzero-ejb/src/main/java/com/findzero/ejb/repository/impl/AuditingRepository.java
+++ findzero-ejb/src/main/java/com/findzero/ejb/repository/impl/AuditingRepository.java
@@ -1,4 +1,16 @@
 package com.findzero.ejb.repository.impl;
 
+import java.util.List;
+
+import com.findzero.ejb.model.Auditing;
+
 public interface AuditingRepository { 
+	
+
+	public List<Auditing> findByProduct(Long idProduct) throws Exception;
+	public Integer countByProduct(Long idProduct) throws Exception;
+	
+	
+	public List<Auditing> findByUser(Long idUser) throws Exception;
+	public Integer countByUser(Long idUser) throws Exception;
 }
--- findzero-ejb/src/main/java/com/findzero/ejb/repository/impl/AuditingRepositoryImpl.java
+++ findzero-ejb/src/main/java/com/findzero/ejb/repository/impl/AuditingRepositoryImpl.java
@@ -1,5 +1,7 @@
 package com.findzero.ejb.repository.impl;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import com.findzero.ejb.model.Auditing;
@@ -11,4 +13,43 @@ public class AuditingRepositoryImpl extends RepositoryBase<Auditing> implements 
 		super(em);
 	}
 
+	@Override
+	public List<Auditing> findByProduct(Long idProduct) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer countByProduct(Long idProduct) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Auditing> findByUser(Long idUser) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer countByUser(Long idUser) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	/*SELECT PARA PEGAR SOMAR O MENSAL DOS USUARIOS PARAMETRO É O MES Q DESEJA VER POR PADRAO SE PASSA O ATUAL
+	 * Select Extract('Month' From a.datelog) as mesatual,
+     a.codusers as codusers,
+     u.name as name,
+     sum(a.points) as points
+From auditing a
+left join users u on u.codusers = a.codusers
+where Extract('Month' From a.datelog) = 3 // Extract('Month' From Now()
+group by Extract('Month' From a.datelog), a.codusers, u.name
+order by  sum(a.points) desc, u.name
+;
+-- Select Extract('Month' From Now()); 
+
+	 */
 }
